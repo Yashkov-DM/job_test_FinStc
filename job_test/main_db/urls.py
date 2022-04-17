cfaffce8601c0dc923dc308afbeb0b72a688e2a3
Yashkov Dmitriy <yashkov-dm@inbox.ru>
2022-04-17 11:41:01 +0300

    2 коммит

--- job_test/main_db/urls.py
+++ job_test/main_db/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main_db, name='main_db'),
+    path('create', views.create, name='create')
 ]
--- job_test/main_db/views.py
+++ job_test/main_db/views.py
@@ -5,3 +5,7 @@ from .models import Customer
 def main_db(request):
     customer = Customer.objects.all()
     return render(request, 'main_db/main_db.html', {'customer': customer})
+
+def create(request):
+    return render(request, 'main_db/create.html')
+
